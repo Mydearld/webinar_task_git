b363ac622dbda30287ea0cf78450fd2c206b54d9
Yana <91089926+Mydearld@users.noreply.github.com>
2026-06-04 01:03:17 +0300

    Update Main.java

--- src/main/java/qa/guru/Main.java
+++ src/main/java/qa/guru/Main.java
@@ -2,6 +2,20 @@ package qa.guru;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello QA!!!!!!!!!!!");
+        // Объявляем переменные
+        int a = 15;
+        int b = 7;
+        
+        // Сложение
+        int sum = a + b;
+        
+        // Вычитание
+        int difference = a - b;
+        
+        // Вывод результатов
+        System.out.println("Число a = " + a);
+        System.out.println("Число b = " + b);
+        System.out.println("Сложение (a + b) = " + sum);
+        System.out.println("Вычитание (a - b) = " + difference);
     }
 }
